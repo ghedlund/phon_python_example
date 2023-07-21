556b011f96dc940b10fd5d14089d644001f63597
Greg Hedlund <greg.hedlund@gmail.com>
2023-07-21 06:59:48 -0230

    Write csv data to file

--- jpype_example.py
+++ jpype_example.py
@@ -2,8 +2,12 @@
 # Example python program using Phon's API via JPype
 #
 # Requirements:
+#   * Install latest version of Phon (https://www.phon.ca) API docs available at https://phon.ca/apidocs/
 #   * Java 17 or later must be installed and available in the system path (recommended: https://adoptium.net/)
 #   * Install JPype (https://jpype.readthedocs.io/en/latest/index.html) (e.g., pip install JPype1)
+#
+# This program will use Phon's IPADictionary to find transcriptions for a list of english words
+# and will create a file 'syllables.csv' which contains a breakdown of syllable types in each word
 #
 import platform
 import csv
@@ -19,7 +23,6 @@ PHON_LIB = PHON_HOME + ("\\lib" if platform.system() == "Windows" else "/Content
 # list of english words
 wordList = ["hello", "world", "goodbye", "sanity"]
 tableHeader = ["Orthography", "IPA", "V", "CV", "VC", "CVC", "Other"]
-writer = csv.writer(sys.stdout)
 
 # Start new JVM instance with Phon classpath
 jpype.startJVM("-ea", classpath=[PHON_LIB + "/*"], convertStrings=True)
@@ -33,36 +36,38 @@ if jpype.isJVMStarted():
     engSyllabifier = SyllabifierLibrary.getInstance().getSyllabifierForLanguage("eng-simple")
 
     IPATranscript = JClass("ca.phon.ipa.IPATranscript")
-    writer.writerow(tableHeader)
-    for word in wordList:
-        options = engDict.lookup(word)
-        for option in options:
-            # Parse transcription
-            ipa = IPATranscript.parseIPATranscript(option)
-            # Add syllable information
-            engSyllabifier.syllabify(ipa.toList())
+    with open('syllables.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(tableHeader)
+        for word in wordList:
+            options = engDict.lookup(word)
+            for option in options:
+                # Parse transcription
+                ipa = IPATranscript.parseIPATranscript(option)
+                # Add syllable information
+                engSyllabifier.syllabify(ipa.toList())
 
-            # setup syllable lists
-            v = ""
-            cv = ""
-            vc = ""
-            cvc = ""
-            other = ""
-            # Break syllables into categories
-            for syllable in ipa.syllables():
-                if syllable.matches("\\s?\\v"):
-                    v += (" " if len(v) > 0 else "") + syllable.toString()
-                elif syllable.matches("\\s?\\c\\v"):
-                    cv += (" " if len(cv) > 0 else "") + syllable.toString()
-                elif syllable.matches("\\s?\\v\\c"):
-                    vc += (" " if len(vc) > 0 else "") + syllable.toString()
-                elif syllable.matches("\\s?\\c\\v\\c"):
-                    cvc += (" " if len(cvc) > 0 else "") + syllable.toString()
-                else:
-                    other += (" " if len(other) > 0 else "") + syllable.toString()
+                # setup syllable lists
+                v = ""
+                cv = ""
+                vc = ""
+                cvc = ""
+                other = ""
+                # Break syllables into categories
+                for syllable in ipa.syllables():
+                    if syllable.matches("\\s?\\v"):
+                        v += (" " if len(v) > 0 else "") + syllable.toString()
+                    elif syllable.matches("\\s?\\c\\v"):
+                        cv += (" " if len(cv) > 0 else "") + syllable.toString()
+                    elif syllable.matches("\\s?\\v\\c"):
+                        vc += (" " if len(vc) > 0 else "") + syllable.toString()
+                    elif syllable.matches("\\s?\\c\\v\\c"):
+                        cvc += (" " if len(cvc) > 0 else "") + syllable.toString()
+                    else:
+                        other += (" " if len(other) > 0 else "") + syllable.toString()
 
-            row = [word, ipa, v, cv, vc, cvc, other]
-            writer.writerow(row)
+                row = [word, ipa, v, cv, vc, cvc, other]
+                writer.writerow(row)
 
     jpype.shutdownJVM()
 else:
